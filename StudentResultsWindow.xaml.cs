2e74c9170ccd1c603368c7894dc9a159b11057df
Blackeyes23 <124727289+Blackeyes23@users.noreply.github.com>
2025-04-10 22:31:48 +0300

    сделал вывод в excel таблицу

--- StudentResultsWindow.xaml.cs
+++ StudentResultsWindow.xaml.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Linq;
+using ClosedXML.Excel;
+using Microsoft.Win32;
+using System.IO;
 using System.Windows;
 using Diplom.Model;
 using Microsoft.EntityFrameworkCore;
@@ -23,6 +26,51 @@ namespace Diplom
                 GroupComboBox.ItemsSource = groups;
                 GroupComboBox.DisplayMemberPath = "Number"; // Показать номер группы
                 GroupComboBox.SelectedValuePath = "Id"; // Использовать Id группы как значение
+            }
+        }
+
+        private void ExportToExcel_Click(object sender, RoutedEventArgs e)
+        {
+            if (GroupComboBox.SelectedItem is Gruppa selectedGroup)
+            {
+                var items = ResultsListView.ItemsSource;
+                if (items == null) return;
+
+                var workbook = new XLWorkbook();
+                var worksheet = workbook.Worksheets.Add("Результаты");
+
+                // Заголовки
+                worksheet.Cell(1, 1).Value = "Фамилия";
+                worksheet.Cell(1, 2).Value = "Имя";
+                worksheet.Cell(1, 3).Value = "Тест";
+                worksheet.Cell(1, 4).Value = "Оценка";
+
+                int row = 2;
+                foreach (dynamic item in items)
+                {
+                    worksheet.Cell(row, 1).Value = item.Surname;
+                    worksheet.Cell(row, 2).Value = item.Name;
+                    worksheet.Cell(row, 3).Value = item.TestName;
+                    worksheet.Cell(row, 4).Value = item.Score;
+                    row++;
+                }
+
+                // Автоподбор ширины столбцов
+                worksheet.Columns().AdjustToContents();
+
+                // Сохранение файла
+                string fileName = $"Аттестация {selectedGroup.Number}.xlsx";
+                SaveFileDialog saveFileDialog = new SaveFileDialog
+                {
+                    FileName = fileName,
+                    Filter = "Excel файлы (*.xlsx)|*.xlsx"
+                };
+
+                if (saveFileDialog.ShowDialog() == true)
+                {
+                    workbook.SaveAs(saveFileDialog.FileName);
+                    MessageBox.Show("Экспорт завершен!", "Успешно", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
             }
         }
 
